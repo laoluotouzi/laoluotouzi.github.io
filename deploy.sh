c9769ce55d68779da5190f1ce1dc7193b9054f19
laoluotouzi <lolooo@163.com>
2023-09-19 21:49:45 +0800

    Last update by 2023-09-19 21:49:45

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-mkdocs build -c
-
 git add -A
 git commit -m "Last update by $(date +"%Y-%m-%d %H:%M:%S")"
 git push
---
+++ gh-deploy.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+mkdocs build -c
+mkdocs gh-deploy
+
